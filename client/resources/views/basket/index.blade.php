2fcfd085ac9eb8c83f61b5448b210f049ee13853
Makarenko Kostya <kochanmujik@gmail.com>
2023-12-04 10:59:06 +0200

    Added product size to the basket table

--- client/resources/views/basket/index.blade.php
+++ client/resources/views/basket/index.blade.php
@@ -9,6 +9,7 @@
                 <th>Изображение</th>
                 <th>Название</th>
                 <th>Количество</th>
+                <th>Размер</th>
                 <th>Цена</th>
                 <th>Сумма</th>
                 <th>Удалить</th>
@@ -26,6 +27,13 @@
                         (<span id="count-{{ $product->id }}">{{ $basketProducts[$product->id] }}</span>)
                         <button style="width: 40px" class="btn btn-danger" data-type="remove" data-id="{{ $product->id }}">-</button>
                         <button style="width: 40px" class="btn btn-success" data-type="add" data-id="{{ $product->id }}">+</button>
+                    </td>
+                    <td>
+                        <select class="form-select">
+                            <option>размер 1</option>
+                            <option>размер 2</option>
+                            <option>размер 2</option>
+                        </select>
                     </td>
                     <td id="productCost-{{ $product->id }}">{{ $product->price }}</td>
                     <td id="productPrice-{{ $product->id }}">{{ $product->price * $basketProducts[$product->id] }}</td>
