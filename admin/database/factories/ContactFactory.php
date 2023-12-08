ac6d5cb28881eaa5b81511597a0a41370f2d01c3
Makarenko Kostya <kochanmujik@gmail.com>
2023-12-08 21:11:06 +0200

    fixed contacts bugs

---
+++ admin/database/factories/ContactFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'company_name' => 'default',
+            'contact_person' => 'default',
+            'address' => 'default',
+            'phone' => 'default'
+        ];
+    }
+}
